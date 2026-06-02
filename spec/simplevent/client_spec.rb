0f9601f64738348e1e5c78ee04440569cd5a96ec
andreip1 <pclandreiuk@gmail.com>
2026-06-02 20:06:25 +0100

    Reword spec

--- spec/simplevent/client_spec.rb
+++ spec/simplevent/client_spec.rb
@@ -3,7 +3,7 @@ require_relative "../../lib/simplevent"
 
 RSpec.describe Simplevent::Client do
   describe ".publish_event" do
-    it "publishes an event to an event stream" do
+    it "publishes an event" do
       expect(subject.publish_event("test_event")).to eq("published")
     end
   end
