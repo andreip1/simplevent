9a7caae1260996c28d26f45d012265de421ff482
andreip1 <pclandreiuk@gmail.com>
2026-06-02 19:12:42 +0100

    First commit

---
+++ lib/simplevent.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simplevent/version"
+
+module Simplevent
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/simplevent/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Simplevent
+  VERSION = "0.1.0"
+end
